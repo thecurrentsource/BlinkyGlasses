031b4b2fd7d3417ecb7a6a9f3e64d654ccc94b98
thecurrentsource <18295991+thecurrentsource@users.noreply.github.com>
2017-11-22 09:12:48 -0500

    Initial upload of source files

---
+++ Keil 8051 v9.53 - Debug/src/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+A51_UPPER_SRCS += \
+../src/SILABS_STARTUP.A51 
+
+C_SRCS += \
+../src/BlinkyGlasses_main.c \
+../src/InitDevice.c \
+../src/Interrupts.c 
+
+OBJS += \
+./src/BlinkyGlasses_main.OBJ \
+./src/InitDevice.OBJ \
+./src/Interrupts.OBJ \
+./src/SILABS_STARTUP.OBJ 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.OBJ: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Compiler'
+	C51 "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/BlinkyGlasses_main.OBJ: C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/EFM8BB1/inc/SI_EFM8BB1_Register_Enums.h C:/Users/derek/SimplicityStudio/v4_workspace/BlinkyGlasses/inc/InitDevice.h C:/Users/derek/SimplicityStudio/v4_workspace/BlinkyGlasses/inc/Equates.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/EFM8BB1/inc/SI_EFM8BB1_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/stdbool.h
+
+src/InitDevice.OBJ: C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/EFM8BB1/inc/SI_EFM8BB1_Register_Enums.h C:/Users/derek/SimplicityStudio/v4_workspace/BlinkyGlasses/inc/InitDevice.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/EFM8BB1/inc/SI_EFM8BB1_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/stdbool.h
+
+src/Interrupts.OBJ: C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/EFM8BB1/inc/SI_EFM8BB1_Register_Enums.h C:/Users/derek/SimplicityStudio/v4_workspace/BlinkyGlasses/inc/Equates.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/EFM8BB1/inc/SI_EFM8BB1_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.6/Device/shared/si8051Base/stdbool.h
+
+src/%.OBJ: ../src/%.A51
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Assembler'
+	AX51 "@$(patsubst %.OBJ,%.__ia,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ inc/Equates.h
@@ -0,0 +1,16 @@
+/*
+ * Equates.h
+ *
+ *  Created on: Aug 14, 2017
+ *      Author: derek
+ */
+
+#ifndef INC_EQUATES_H_
+#define INC_EQUATES_H_
+
+#include <SI_EFM8BB1_Register_Enums.h>
+
+SI_SBIT(LEFTEYE, SFR_P0, 0);
+SI_SBIT(RIGHTEYE, SFR_P1, 0);
+
+#endif /* INC_EQUATES_H_ */
